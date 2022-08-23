9780fa87e055e671e432a62885c5c4b5be1d7cbb
ghedrick76 <graysonh76@icloud.com>
2022-08-23 19:56:13 -0400

    fixed index functions

--- functions.py
+++ functions.py
@@ -1,36 +1,31 @@
 import pandas as pd
-import pandas_datareader as pdr
 import yfinance as yf
+from pandas_datareader import data as pdr
 
-input = input("Choose your stock symbol: ")
+yf.pdr_override()
 
-data = yf.Ticker(input).history(period='5y')
+# input = input("Choose your stock symbol: ")
 
-print(data.info())
+# data = yf.Ticker(input).history(period='5y')
 
+# print(data.info())
+
+
+# Create the indices
 def dow_jones():
-
-    data = yf.Ticker("DJI").history(period='10y')
-
-    return data.info()
-
-dow_jones()
+    data = pdr.get_data_yahoo("DJI", start="2011-01-01", end="2012-08-20")
+    return data
 
 def nasdaq():
-    
     data = yf.Ticker("IXIC").history(period='10y')
 
     return data.info()
 
-nasdaq()
-
 def sp500():
-    
     data = yf.Ticker("GSPC").history(period='10y')
 
     return data.info()
 
-sp500()
 
 
 #Make a function for 20-day, 50-day, 100-day, 200-day moving averages, using the above to change the timeframe
