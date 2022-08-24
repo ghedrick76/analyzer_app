eae15e094d60b8ce179eed8f9f34b5f570ba0625
ghedrick76 <graysonh76@icloud.com>
2022-08-23 20:27:13 -0400

    close function

--- functions.py
+++ functions.py
@@ -7,18 +7,22 @@ yf.pdr_override()
 
 # Create DataFrames for the benchmark indices
 def dow_jones():
-    data = pdr.get_data_yahoo("DJI", period="10y")["Close"]
+    data = pdr.get_data_yahoo("DJI", period="10y")#["Close"]
     return data
 
 def nasdaq():
-    data = pdr.get_data_yahoo("IXIC", period="10y")
-    return data
+    ixic = pdr.get_data_yahoo("IXIC", period="10y")
+    return ixic
 
 def sp500():
     data = pdr.get_data_yahoo("GSPC", period="10y")
     return data
 
 
+
+def close(self):
+    self = self["Close"]
+    return self
 
 #Make a function for 20-day, 50-day, 100-day, 200-day moving averages, using the above to change the timeframe
 
