5711ad629b96f06293406a4a793a1350e6c82a51
ghedrick76 <graysonh76@icloud.com>
2022-08-23 20:17:21 -0400

    editing index functions

--- functions.py
+++ functions.py
@@ -2,18 +2,12 @@ import pandas as pd
 import yfinance as yf
 from pandas_datareader import data as pdr
 
+# Allow yfinance to fetch DataFrames
 yf.pdr_override()
 
-# input = input("Choose your stock symbol: ")
-
-# data = yf.Ticker(input).history(period='5y')
-
-# print(data.info())
-
-
-# Create the indices
+# Create DataFrames for the benchmark indices
 def dow_jones():
-    data = pdr.get_data_yahoo("DJI", period="10y")
+    data = pdr.get_data_yahoo("DJI", period="10y")["Close"]
     return data
 
 def nasdaq():
@@ -27,3 +21,12 @@ def sp500():
 
 
 #Make a function for 20-day, 50-day, 100-day, 200-day moving averages, using the above to change the timeframe
+
+
+
+
+# input = input("Choose your stock symbol: ")
+
+# data = yf.Ticker(input).history(period='5y')
+
+# print(data.info())
