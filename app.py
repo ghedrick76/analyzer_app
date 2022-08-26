aa01fc778df492c41f10a9f68899613c898da1f3
ghedrick76 <graysonh76@icloud.com>
2022-08-26 19:25:38 -0400

    added beta calculation

--- app.py
+++ app.py
@@ -4,10 +4,11 @@
 # In[1]:
 
 
-import pandas as pd # for dataframes
-import numpy as np # for NaN
+# Import required libraries
+import pandas as pd
+import numpy as np
 import yfinance as yf
-import matplotlib.pyplot as plt # for visualization
+import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
@@ -25,10 +26,12 @@ tickers = pd.concat([nyse,nasdaq]).drop_duplicates().reset_index(drop=True)
 # Create the Streamlit instance
 st.title('Finance Dashboard')
 
-# Creates the dropdown box for the user to select tickers
+# Creates the dropdown boxes for the user to select their tickers and desired functions
 dropdown = st.multiselect('Select equities to chart', tickers)
 
 dropdown2 = st.multiselect('Calculate volatility ', tickers)
+
+dropdown3 = st.multiselect('Calculate the beta of any two tickers', tickers)
 
 
 # Sets the start and end dates
@@ -45,15 +48,27 @@ end = st.date_input('End', value = pd.to_datetime('today'))
 #     cumret = cumret.fillna(0)
 #     return cumret
 
+# Build functions for user actions
+
+# Graphs the ticker
 def returns(df):
     returns = df
     return returns
 
+# Calculates volatility of a ticker
 def calc_vol(df):
     df = df.pct_change()
     volatility = df.std()*252**.5
     return volatility
 
+# Calculates beta of a ticker against whatever ticker is in the first position
+def calc_beta(df):
+    covariance = df.cov()
+    variance = df[dropdown3[1]].var()
+    covariance = covariance.loc[dropdown3[0], dropdown3[1]]/variance
+    return covariance
+
+# Conditionals for each desired function
 if len(dropdown) > 0:
     df = returns(yf.download(dropdown,start,end)['Close'])
     st.header('Graph of {}'.format(dropdown))
@@ -63,6 +78,11 @@ if len(dropdown2) > 0:
     df = calc_vol(yf.download(dropdown2,start,end)['Close'])
     st.header('Volatility of {}'.format(dropdown2))
     df
+    
+if len(dropdown3) > 1:
+    df = calc_beta(yf.download(dropdown3,start,end)['Close'])
+    st.header("The beta of {} compared against {} is".format(dropdown3[1], dropdown3[0]))
+    st.header(df)
     
 
 
