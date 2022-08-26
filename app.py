cb2e38063cef344940e1f9feb870f658cb12ba57
ghedrick76 <graysonh76@icloud.com>
2022-08-25 21:18:41 -0400

    fixed close price

--- app.py
+++ app.py
@@ -16,20 +16,18 @@ nyse = pd.read_csv("untitled.csv")['ACT Symbol']
 nasdaq = pd.read_csv("nasdaq.csv")['Symbol']
 
 tickers = pd.concat([nyse,nasdaq]).drop_duplicates().reset_index(drop=True)
-#tickers = nyse.append(nasdaq)
-#tickers.drop_duplicates(keep='last')
-tickers
 
 
 # In[2]:
 
 
+# Create the Streamlit instance
 st.title('Finance Dashboard')
 
-#tickers = st.input("Select your tickers")
-
+# Creates the dropdown box for the user to select tickers
 dropdown = st.multiselect('Pick your assets', tickers)
 
+# Sets the start and end dates
 start = st.date_input('Start', value = pd.to_datetime('2012-01-01'))
 end = st.date_input('End', value = pd.to_datetime('today'))
 
@@ -37,16 +35,21 @@ end = st.date_input('End', value = pd.to_datetime('today'))
 # In[3]:
 
 
+# def relativeret(df):
+#     rel = df.pct_change()
+#     cumret = (1+rel).cumprod() - 1
+#     cumret = cumret.fillna(0)
+#     return cumret
+
 def relativeret(df):
-    rel = df.pct_change()
-    cumret = (1+rel).cumprod() - 1
-    cumret = cumret.fillna(0)
-    return cumret
+    rel = df
+    return rel
 
 if len(dropdown) > 0:
     df = relativeret(yf.download(dropdown,start,end)['Close'])
     st.header('Returns of {}'.format(dropdown))
     st.line_chart(df)
+    
 
 
 # # What does the script do? 
